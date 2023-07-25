132914cce0a35f3feaefe8a5f1db7debb905399f
Omit-pyton <135300835+Omit-pyton@users.noreply.github.com>
2023-07-25 06:50:25 +0300

    Update Homework 1.py

--- Homework 1.py
+++ Homework 1.py
@@ -8,11 +8,11 @@ class Rectangle:
         self.w = w
 
     def area (self, h, w):
-        rectangle_area = self.h * self.w
+        rectangle_area = h * w
         print(f"Rectangle area is {rectangle_area} ")
 
     def pergmeter(self, h, w):
-        rectangle_pergmeter = (self.h + self.w) * 2
+        rectangle_pergmeter = (h + w) * 2
         print(f"Rectangle pergmeter is {rectangle_pergmeter} ")
 
 h = int(input("Enter rectangle height: "))
